ca1cbc162d7e7663c0f67e0d3371547eb3e950e2
MateDeCalabaza <aribraver3@hotmail.com>
2025-02-01 19:25:09 -0300

    hago un commit luego de 2 meses de stand by

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 #Crear rutas con sus correspondientes funciones
 #En flask, a los decoradores se los conoce como "pocesador de contexto" y lo que sigue se trata de una RUTA.
 #Entre paréntesis se pone una barra invertida como parámetro, indicando que es la ruta inicial, es decir, la
-#página de incio. 
+#página de incio. Luego se genera una función que determina sucederá al ingresar a esa URL.
 @app.route('/')
 def holamundo():
     return 'Hola Mundo!'
@@ -18,5 +18,6 @@ def mostrarproyectos():
 
 #Ejecutar nuestra app cuando ejecutemos este archivo run.py
 #A su vez, al ejecutar en modo "debug=true" lo estamos haciendo en modo de desarrollo
+#Los parámetros que se utilizan son el host, el puerto y si lo usamos en modo debug o no.
 if __name__ == '__main__':
     app.run(debug=True)
