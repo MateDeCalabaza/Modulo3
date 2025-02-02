6afb262b8efd4d055945d708e24a5eb21d042ad4
MateDeCalabaza <aribraver3@hotmail.com>
2025-02-02 02:14:11 -0300

    Generé nuevas rutas con sus respectivos templates vacíos

--- run.py
+++ run.py
@@ -1,21 +1,34 @@
-from flask import Flask
+from flask import Flask, render_template
 
 #Crear una instancia de la clase Flask, llamada "app"
 app = Flask(__name__)
 
-#Crear rutas con sus correspondientes funciones
-#En flask, a los decoradores se los conoce como "pocesador de contexto" y lo que sigue se trata de una RUTA. Lo que hicimos acá es llamar a un método de la instancia "app"
+#Crear rutas con sus correspondientes funciones:
+#En flask, a los decoradores se los conoce como "pocesador de contexto" y lo que sigue se trata de una RUTA. Lo que hicimos acá es llamar a un método de la instancia "app".
 #Entre paréntesis se pone una barra invertida como parámetro, indicando que es la ruta inicial, es decir, la
-#página de incio. Al lado se pone como argumento los tipos de solicitudes que puede hacerle el cliente al servidor, para luego otorgarle una respuesta. Luego se genera una función que determina qué sucederá al ingresar a esa URL.
+#página de incio. Al lado se pone como argumento los tipos de solicitudes que puede hacerle el cliente al servidor, para luego otorgarle una respuesta. Luego se genera una función que determina qué sucederá al ingresar a esa URL, en este caso se utiliza la función "render_template" para redirigir a el archivo "index.html" que se encuentra dentro de la carpeta "templates".
 #Existen muchos tipos de solicitudes: GET, HEAD, POST, PUT, DELETE, CONNECT, OPTIONs, TRACE o PATCH 
+
+#INICIO
 @app.route('/', methods=["GET"])
 def holamundo():
-    return 'Hola Mundo!'
+    return render_template("/index.html")
 
-#Acá agregamos otra ruta, que como vemos dentro del paréntesis, tiene su dirección dentro de la ruta inicial.
+#MIS PROYECTOS
 @app.route('/mis-proyectos', methods=["GET"])
 def mostrarproyectos():
-    return 'Aquí se mostrarán mis proyectos'
+    return render_template("/mis_proyectos.html")
+
+#CONTACTO
+@app.route("/contacto", methods=["GET"])
+def contacto():
+    return render_template("/contacto.html")
+
+#BLOG
+@app.route("/blog", methods=["GET"])
+def mi_blog():
+    return render_template("/blog.html")
+
 
 #Ejecutar nuestra app cuando ejecutemos este archivo run.py
 #A su vez, al ejecutar en modo "debug=true" lo estamos haciendo en modo de desarrollo
